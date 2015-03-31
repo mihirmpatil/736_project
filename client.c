049e080ee20e7439958085483123561412c116b1
mihir <mihir@node-0.mihir-qv3970.varysproject-pg0.apt.emulab.net>
2015-03-31 10:18:28 -0600

    daemonized client

--- client.c
+++ client.c
@@ -6,14 +6,83 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <syslog.h>
+
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+static void daemonize(void)
+{
+    pid_t pid, sid;
+    int fd; 
+
+    /* already a daemon */
+    if ( getppid() == 1 ) return;
+
+    /* Fork off the parent process */
+    pid = fork();
+    if (pid < 0)  
+    {   
+        exit(EXIT_FAILURE);
+    }   
+
+    if (pid > 0)  
+    {   
+        exit(EXIT_SUCCESS); /*Killing the Parent Process*/
+    }   
+
+    /* At this point we are executing as the child process */
+
+    /* Create a new SID for the child process */
+    sid = setsid();
+    if (sid < 0)  
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    /* Change the current working directory. */
+    if ((chdir("/")) < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+
+    fd = open("/dev/null",O_RDWR, 0);
+
+    if (fd != -1)
+    {
+        dup2 (fd, STDIN_FILENO);
+        dup2 (fd, STDOUT_FILENO);
+        dup2 (fd, STDERR_FILENO);
+
+        if (fd > 2)
+        {
+            close (fd);
+        }
+    }
+
+    /*resettign File Creation Mask */
+    umask(027);
+    openlog("client", LOG_PID, LOG_DAEMON);
+}
+
+
 
 void error(const char *msg)
 {
-    perror(msg);
-    exit(0);
+    syslog(LOG_ERR,msg);
+//    exit(0);
 }
 
 int main(int argc, char *argv[])
+{
+
+daemonize();
+syslog(LOG_NOTICE, "daemonize complete");
+while (1)
 {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
@@ -41,17 +110,22 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    printf("Please enter the message: ");
+//    printf("Please enter the message: ");
     bzero(buffer,256);
-    fgets(buffer,255,stdin);
+    strcpy(buffer, "THis is a test message\n");
+//    fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
+syslog(LOG_NOTICE, "Written data to socket");
     if (n < 0) 
          error("ERROR writing to socket");
     bzero(buffer,256);
     n = read(sockfd,buffer,255);
     if (n < 0) 
          error("ERROR reading from socket");
-    printf("%s\n",buffer);
+syslog(LOG_NOTICE, "Read data from socket");
+//    printf("%s\n",buffer);
     close(sockfd);
+    sleep(2);	//give back cpu
+}
     return 0;
 }
